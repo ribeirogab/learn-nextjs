f97371744d67578a36fd44860a20ced2dff1f560
ribeirogab <gblosr@gmail.com>
2020-10-28 23:47:51 -0300

    client fide fetching with SWR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
+import useSWR from 'swr'
+
 import * as S from '../styles/pages/Home'
 
+interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+  category_id: string;
+  slug: string;
+}
+
 export default function Home() {
-  return <S.Title>Home</S.Title>
+  const  { data: products, error } = useSWR<IProduct[]>('http://localhost:3333/recommended');
+
+  console.log(products);
+
+  return (
+    <>
+      <S.Title>
+        {error && 'failed to load'}
+        {!products ? 'loading...' : 'Produtos Recomendados:'}
+      </S.Title>
+      <ul>
+        {products?.map(product => (
+          <li>- {product.title}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
